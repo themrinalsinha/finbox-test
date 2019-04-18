f1d00c3d93a66137095a9d0eb4981e20054aa6bf
Mrinal Sinha <themrinalsinha@gmail.com>
2019-04-18 19:44:00 +0530

    models created for data

--- webapp/models.py
+++ webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class FoodReviews(models.Model):
+    product_id  = models.CharField(max_length=16)
+    user_id     = models.CharField(max_length=16)
+    name        = models.CharField(max_length=32)
+    helpfulness = models.CharField(max_length=8)
+    score       = models.DecimalField(max_digits=4, decimal_places=2)
+    timestamp   = models.DateTimeField(blank=True, null=True)
+    summary     = models.CharField(max_length=256)
+    text        = models.TextField()
+
+    def __str__(self):
+        return '{} - review by {} ({})'.format(self.product_id, self.name, self.user_id)
+
+    class Meta:
+        verbose_name        = 'Food Review'
+        verbose_name_plural = 'Food Reviews'
