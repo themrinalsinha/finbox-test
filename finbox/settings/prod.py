b33d2f724abcee856174a6eb1fcc169f1c923fd8
Mrinal Sinha <themrinalsinha@gmail.com>
2019-04-19 18:28:26 +0530

    fetching latest git id

--- finbox/settings/prod.py
+++ finbox/settings/prod.py
@@ -1,6 +1,7 @@
-from .base import *
+from .base      import *
+from subprocess import check_output
 
 DEBUG         = False
 SECRET_KEY    = '1eeb96e4-d97b-4610-b383-9b1bcd7a2edf'
 ALLOWED_HOSTS = ['finbox.themrinalsinha.com']
-GIT_COMMIT_ID = None
+GIT_COMMIT_ID = check_output("git describe --tags --always --long", shell=True).strip()
