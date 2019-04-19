608887ba8a8ac9b0e568ca7f68446a93088aaedc
Mrinal Sinha <themrinalsinha@gmail.com>
2019-04-19 18:17:22 +0530

    minor bug fixes

--- webapp/views.py
+++ webapp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts     import render
 from django.views.generic import TemplateView
 from django.http          import HttpResponseRedirect, JsonResponse
+from django.conf          import settings
+from os.path              import exists, join
 
 from .utils               import FinboxSearch
 from .models              import FoodReviews
@@ -14,6 +16,10 @@ class HomeView(TemplateView):
 
     def post(self, request, *args, **kwargs):
         context = self.get_context_data()
+        if not exists(join(settings.PROJECT_DIR, 'finbox.db')) or not exists(join(settings.INV_INDEX)):
+            context['db_missing'] = True
+            return self.render_to_response(context)
+
         search_text = request.POST.get('search_text', None)
         context['search_string'] = search_text if search_text else ''
         if search_text:
