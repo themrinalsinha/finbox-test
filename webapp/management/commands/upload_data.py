404b5c0771f426099dcd858623553b7d78d6b7f1
Mrinal Sinha <themrinalsinha@gmail.com>
2019-04-19 10:58:54 +0530

    structure modifications

--- webapp/management/commands/upload_data.py
+++ webapp/management/commands/upload_data.py
@@ -1,39 +1,10 @@
 from django.core.management.base import BaseCommand
 from django.conf                 import settings
-from collections                 import Counter
-from datetime                    import datetime
 from os.path                     import exists
-from string                      import punctuation
 from yaml                        import dump, load
 
+from webapp.utils                import _to_date, _clean_text
 from webapp.models               import FoodReviews
-
-# Helper functions
-def _to_date(d):
-    return datetime.fromtimestamp(d) if d else None
-
-def _clean_text(text):
-    text = text.lower()
-    stop_words = [ "a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
-                   "any", "are", "as", "at", "be", "because", "been", "before", "being", "below",
-                   "between", "both", "but", "by", "could", "did", "do", "does", "doing", "down",
-                   "during", "each", "few", "for", "from", "further", "had", "has", "have", "having",
-                   "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself", "him",
-                   "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if", "in",
-                   "into", "is", "it", "it's", "its", "itself", "let's", "me", "more", "most", "my",
-                   "myself", "nor", "of", "on", "once", "only", "or", "other", "ought", "our", "ours",
-                   "ourselves", "out", "over", "own", "same", "she", "she'd", "she'll", "she's", "should",
-                   "so", "some", "such", "than", "that", "that's", "the", "their", "theirs",
-                   "them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll",
-                   "they're", "they've", "this", "those", "through", "to", "too", "under", "until", "up",
-                   "very", "was", "we", "we'd", "we'll", "we're", "we've", "were", "what", "what's", "when",
-                   "when's", "where", "where's", "which", "while", "who", "who's", "whom", "why", "why's",
-                   "with", "would", "you", "you'd", "you'll", "you're", "you've", "your", "yours", "yourself",
-                   "yourselves"]
-
-    text = text.translate(text.maketrans('', '', punctuation))
-    text = [x.strip() for x in text.split(' ') if x and x not in stop_words]
-    return Counter(text)
 
 class Command(BaseCommand):
     def add_arguments(self, parser):
--- webapp/views.py
+++ webapp/views.py
@@ -1,5 +1,11 @@
-from django.shortcuts     import render
+from django.shortcuts     import render, reverse
 from django.views.generic import TemplateView
+
+from django.http          import HttpResponseRedirect
 
 class HomeView(TemplateView):
     template_name = 'index.html'
+
+    def post(self, request, *args, **kwargs):
+        search_text = request.POST.get('search_text')
+        return HttpResponseRedirect(reverse('home'))
